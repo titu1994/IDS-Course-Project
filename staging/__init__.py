f6460aab6bf342a914f1e372ab0d7844a77d7444
Somshubra Majumdar <titu1994@gmail.com>
2018-02-14 15:57:19 -0600

    Improve __init__.py for speed

--- staging/__init__.py
+++ staging/__init__.py
@@ -1,10 +1,3 @@
 # Create convenience links to the construct and resolve methods for data paths
 from staging.utils.generic_utils import resolve_data_path
 from staging.utils.generic_utils import construct_data_path
-
-# Import custom layers directly
-from staging.utils.layers.nascell import NASCell
-from staging.utils.layers.attention_lstm import AttentionLSTM
-from staging.utils.layers.multiplicative_lstm import MultiplicativeLSTM
-from staging.utils.layers.nested_lstm import NestedLSTM
-from staging.utils.layers.minimal_rnn import MinimalRNN
