78188ee77895c745108c17a8dba078ee574b5ac8
Somshubra Majumdar <titu1994@gmail.com>
2018-04-26 22:46:28 -0500

    Remove ml sentiment from servers

--- staging/servers/ratings.py
+++ staging/servers/ratings.py
@@ -4,7 +4,7 @@ sys.path.insert(0, "..")
 import flask
 from flask import request
 
-from staging.ml_eval import predict_ml_ratings
+# from staging.ml_eval import predict_ml_ratings
 from staging.dl_eval import predict_dl_ratings
 
 # initialize the server
@@ -14,7 +14,7 @@ app = flask.Flask(__name__)
 print("Please wait while the models are being initialized for fast inference ! \n"
       "This may take several minutes...")
 
-predict_ml_ratings._initialize()
+# predict_ml_ratings._initialize()
 predict_dl_ratings._initialize()
 
 print("Server is ready !")
@@ -25,21 +25,21 @@ def default():
     return "Rating Server Initialized"
 
 
-@app.route('/rating/ml', methods=['POST'])
-def predict_rating_ml():
-    if request.json is None:
-        return flask.abort(400)
-
-    if 'model_name' not in request.json:
-        return flask.Response('"model_name" not provided in request !')
-
-    if 'query' not in request.json:
-        return flask.Response('"query" string not provided in request !')
-
-    ml_model = str(request.json['model_name']).lower()
-    query = request.json['query']
-
-    return _resolve_ml_query(ml_model, query)
+# @app.route('/rating/ml', methods=['POST'])
+# def predict_rating_ml():
+#     if request.json is None:
+#         return flask.abort(400)
+#
+#     if 'model_name' not in request.json:
+#         return flask.Response('"model_name" not provided in request !')
+#
+#     if 'query' not in request.json:
+#         return flask.Response('"query" string not provided in request !')
+#
+#     ml_model = str(request.json['model_name']).lower()
+#     query = request.json['query']
+#
+#     return _resolve_ml_query(ml_model, query)
 
 
 @app.route('/rating/dl', methods=['POST'])
@@ -59,23 +59,23 @@ def predict_rating_dl():
     return _resolve_dl_query(ml_model, query)
 
 
-def _resolve_ml_query(model_name, query):
-    if model_name not in ['tree', 'logistic', 'forest']:
-        return flask.Response('Incorrect ml model name. Must be one of ["tree", "logistic", "forest"')
-
-    if model_name == 'tree':
-        rating, confidence = predict_ml_ratings.get_decision_tree_rating_prediction(query)
-    elif model_name == 'logistic':
-        rating, confidence = predict_ml_ratings.get_logistic_regression_rating_prediction(query)
-    else:
-        rating, confidence = predict_ml_ratings.get_random_forest_rating_prediction(query)
-
-    response = {
-        'rating': int(rating[0]),
-        'confidence': confidence[0],
-    }
-
-    return flask.jsonify(**response)
+# def _resolve_ml_query(model_name, query):
+#     if model_name not in ['tree', 'logistic', 'forest']:
+#         return flask.Response('Incorrect ml model name. Must be one of ["tree", "logistic", "forest"')
+#
+#     if model_name == 'tree':
+#         rating, confidence = predict_ml_ratings.get_decision_tree_rating_prediction(query)
+#     elif model_name == 'logistic':
+#         rating, confidence = predict_ml_ratings.get_logistic_regression_rating_prediction(query)
+#     else:
+#         rating, confidence = predict_ml_ratings.get_random_forest_rating_prediction(query)
+#
+#     response = {
+#         'rating': int(rating[0]),
+#         'confidence': confidence[0],
+#     }
+#
+#     return flask.jsonify(**response)
 
 
 def _resolve_dl_query(model_name, query):
--- staging/servers/sentiment.py
+++ staging/servers/sentiment.py
@@ -4,7 +4,7 @@ sys.path.insert(0, "..")
 import flask
 from flask import request
 
-from staging.ml_eval import predict_ml_sentiment
+# from staging.ml_eval import predict_ml_sentiment
 from staging.dl_eval import predict_dl_sentiment
 
 # initialize the server
@@ -14,7 +14,7 @@ app = flask.Flask(__name__)
 print("Please wait while the models are being initialized for fast inference ! \n"
       "This may take several minutes...")
 
-predict_ml_sentiment._initialize()
+# predict_ml_sentiment._initialize()
 predict_dl_sentiment._initialize()
 
 print("Server is ready !")
@@ -25,21 +25,21 @@ def default():
     return "Sentiment Server Initialized"
 
 
-@app.route('/sentiment/ml', methods=['POST'])
-def predict_sentiment_ml():
-    if request.json is None:
-        return flask.abort(400)
-
-    if 'model_name' not in request.json:
-        return flask.Response('"model_name" not provided in request !')
-
-    if 'query' not in request.json:
-        return flask.Response('"query" string not provided in request !')
-
-    ml_model = str(request.json['model_name']).lower()
-    query = request.json['query']
-
-    return _resolve_ml_query(ml_model, query)
+# @app.route('/sentiment/ml', methods=['POST'])
+# def predict_sentiment_ml():
+#     if request.json is None:
+#         return flask.abort(400)
+#
+#     if 'model_name' not in request.json:
+#         return flask.Response('"model_name" not provided in request !')
+#
+#     if 'query' not in request.json:
+#         return flask.Response('"query" string not provided in request !')
+#
+#     ml_model = str(request.json['model_name']).lower()
+#     query = request.json['query']
+#
+#     return _resolve_ml_query(ml_model, query)
 
 
 @app.route('/sentiment/dl', methods=['POST'])
@@ -58,30 +58,30 @@ def predict_sentiment_dl():
 
     return _resolve_dl_query(ml_model, query)
 
-
-def _resolve_ml_query(model_name, query):
-    if model_name not in ['tree', 'logistic', 'forest']:
-        return flask.Response('Incorrect ml model name. Must be one of ["tree", "logistic", "forest"')
-
-    if model_name == 'tree':
-        sentiment, confidence = predict_ml_sentiment.get_decision_tree_sentiment_prediction(query)
-    elif model_name == 'logistic':
-        sentiment, confidence = predict_ml_sentiment.get_logistic_regression_sentiment_prediction(query)
-    else:
-        sentiment, confidence = predict_ml_sentiment.get_random_forest_sentiment_prediction(query)
-
-    if sentiment == 0:
-        sentiment = 'Negative'
-    else:
-        sentiment = 'Positive'
-
-    response = {
-        'sentiment': sentiment[0],
-        'confidence': float(confidence[0]),
-    }
-
-    return flask.jsonify(**response)
-
+#
+# def _resolve_ml_query(model_name, query):
+#     if model_name not in ['tree', 'logistic', 'forest']:
+#         return flask.Response('Incorrect ml model name. Must be one of ["tree", "logistic", "forest"')
+#
+#     if model_name == 'tree':
+#         sentiment, confidence = predict_ml_sentiment.get_decision_tree_sentiment_prediction(query)
+#     elif model_name == 'logistic':
+#         sentiment, confidence = predict_ml_sentiment.get_logistic_regression_sentiment_prediction(query)
+#     else:
+#         sentiment, confidence = predict_ml_sentiment.get_random_forest_sentiment_prediction(query)
+#
+#     if sentiment == 0:
+#         sentiment = 'Negative'
+#     else:
+#         sentiment = 'Positive'
+#
+#     response = {
+#         'sentiment': sentiment[0],
+#         'confidence': float(confidence[0]),
+#     }
+#
+#     return flask.jsonify(**response)
+#
 
 def _resolve_dl_query(model_name, query):
     if model_name not in ['lstm', 'mult_lstm', 'malstm_fcn']:
