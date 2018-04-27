1071f845614e0af8f2f10e1a80f4e2a1940d9f60
Somshubra Majumdar <titu1994@gmail.com>
2018-04-27 11:11:00 -0500

    Push GUI server

---
+++ staging/display_server.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.insert(0, "..")
+
+from staging.servers.gui import app as application
+
+if __name__ == '__main__':
+    application.run()
---
+++ staging/servers/gui.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify, render_template, request
+from flask_cors import CORS
+import requests
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+API_URL_SENTIMENT = 'http://35.193.72.219:9000/sentiment/dl'
+API_URL_RATINGS = 'http://35.193.72.219:9001/rating/dl'
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/sentiment/dl', methods=['POST'])
+def sentiment():
+    print("Message", request.form['message'])
+    post_fields = {'model_name': 'lstm',
+                   'query': request.form['message']}
+
+    sentiment = requests.post(API_URL_SENTIMENT, json=post_fields)
+    # rating = requests.post(API_URL_RATINGS, json=post_fields)
+
+    sentiment = json.loads(sentiment.text)
+    # rating = json.loads(rating.text)
+
+    sentiment_class, sentiment_confidence = sentiment['sentiment'], sentiment['confidence']
+    # rating_class, rating_confidence = int(rating['rating']) + 1, rating['confidence']
+
+    response = {'polarity': sentiment_confidence, 'subjectivity': sentiment_class}
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=9000, debug=True)
